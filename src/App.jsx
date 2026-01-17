45faf75d6aaeffc5d7dd04846a06353d7648ca63
ducsaco123 <ducnguyen230705@gmail.com>
2026-01-17 13:41:07 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Footer from './exe1/Footer'
+import Navigation from './exe1/Navigation'
+import Pets from './exe1/Pets'
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+      <Pets />
+      <Footer />
+    </>
+  )
+}
+
+export default App
